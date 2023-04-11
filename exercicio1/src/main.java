676bdfc3fced02fc1638d711b3153f392cf744c3
Nathan-Barbosa <nathan.s.barbosa01@gmail.com>
2023-04-11 16:28:59 -0300

    Lista 3 de Java

---
+++ exercicio1/src/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import model.Produto;
+
+public class main {
+  public static void main(String ...args){
+
+    try (Scanner scan = new Scanner(System.in)) {
+      System.out.println("Informe o codigo, a quantidade o valor unitario e a forma de pagamento: ");
+      int codigo = scan.nextInt();
+      int qnt = scan.nextInt();
+      Double valorUnitario = scan.nextDouble();
+      int formaPagamento = scan.nextInt();
+      
+      Produto p1 = new Produto(codigo, qnt, valorUnitario, formaPagamento);
+      p1.conta();
+    }
+  }  
+}
